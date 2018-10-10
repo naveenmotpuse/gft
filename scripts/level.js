20fbc334c95db53c9432a4e9a9c3c0255668f65b
devananddhage <devanand.dhage@gmail.com>
2018-10-10 14:07:48 +0530

    ok

--- scripts/level.js
+++ scripts/level.js
@@ -50,8 +50,8 @@ var _LevelAccess = (function() {
     SetVisibleLevels: function(visLev) {
       if(typeof visLev != undefined && !_Common.IsEmptyObject(visLev)) {
         for(var i=0;i<=Object.keys(visLev).length;i++) {
-          visibleLevels[i] = typeof visLev["level" + i] === 'undefined' ? true : visLev["level" + i];
-          visLev["level" + i];
+          visLev[i] = typeof visLev["level" + i] === 'undefined' ? true : visLev["level" + i];
+          delete visLev["level" + i];
         }
       }
       visibleLevels = _Common.IsEmptyObject(visLev) ? visibleLevels : visLev;
